7b556e78c92e7e6e303cdc3054166bf7fe589862
notmycontent <129483274+notmycontent@users.noreply.github.com>
2023-04-18 15:34:45 +0700

    Add files via upload

---
+++ opening.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Script by LawNET"
+sleep 5
+
+rm /etc/ssh/sshd_config
+sleep 3
+
+cd /etc/ssh
+ls
+sleep 5
+
+wget https://raw.githubusercontent.com/notmycontent/setup/main/sshd_config
+sleep 3
+
+service ssh restart
+service sshd restart
+sleep 5
+
+apt update
+apt install vnstat -y
+apt install htop -y
+apt upgrade -y
+sleep 5
+
+cd
+rm opening.sh
+
+reboot
+
